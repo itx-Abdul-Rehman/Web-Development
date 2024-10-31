fa2f23e2272bdb792d0836f9d686718ecdbd40d2
Mohammad Abdul Rehman <itsrehman2004@gmail.com>
2024-10-31 17:16:30 +0500

    Prac 19

---
+++ Prac-19/script.js
@@ -0,0 +1,36 @@
+
+console.log("Hello world");
+
+
+//--------Practice set 6--------
+
+//-------- Q1 -----------
+ function getInput(){
+    let age=prompt("Enter age");
+if(age>=18){
+  alert("He can drive");
+}else{
+  alert("Not drive");
+}
+ }
+
+ getInput();
+
+//-------- Q2 ---------
+let isAgain=confirm("Do you want to put again age?");
+if(isAgain){
+   getInput();
+}
+
+//----- Q5 ---------
+let a=prompt("Enter Color");
+document.body.style.backgroundColor=a;
+
+//----- Q3 ----------
+console.error(age);
+
+//----- Q4 ---------
+if(age>4){
+    location.href="https/:www.facebook.com/";
+}
+
